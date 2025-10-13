ab870b02b394f88fe03b2f48acf0ccda006ee598
kseniaksyy <kseniaksyyy@gmail.com>
2025-10-13 21:45:35 +0300

    Cypress intro

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'sq9tsq',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
